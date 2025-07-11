ce2c8f0ad48ae3185d32169c2f37c3aea0b42622
tchakoute2001 <146494365+tchakoute2001@users.noreply.github.com>
2025-07-11 14:24:50 +0200

    Create GameConstants.java

---
+++ Src/Tchakoute/GameConstants.java
@@ -0,0 +1,10 @@
+package tchakoute;
+
+public class GameConstants {
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int BALL_DIAMETER = 20;
+    public static final int PADDLE_WIDTH = 100;
+    public static final int PADDLE_HEIGHT = 15;
+	public static final int BRICK_ROWS = 0;
+}
